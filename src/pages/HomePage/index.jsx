e73767d7cfeef2bc15dbf77a086572bf598269eb
nlacerdagabriel <devlacerdagabriel@hotmail.com>
2023-02-22 23:42:25 -0300

    ,

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,7 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
-import { Courses } from "../../Sections/Courses";
-import { Home } from "../../Sections/Home";
+import { Courses } from "../../sections/Courses";
+import { Home } from "../../sections/Home";
 
 export const HomePage = () => {
   return (
